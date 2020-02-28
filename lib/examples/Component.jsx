2972216ede8f2312b0271b315629bb3bd8ad771c
Rodrigo Oler <roodrigoprogrammer@gmail.com>
2020-02-28 01:30:33 -0300

    create project

---
+++ lib/examples/Component.jsx
@@ -0,0 +1,19 @@
+import React, { useState } from 'react'
+
+export default function UserForm() {
+    const [state] = useState({
+        name: 'Rodrigo',
+        email: 'oler42@fakemail.now',
+        password: '**********',
+    })
+
+    const {name, email, password} = state;
+
+    return (
+        <p>
+            {name}
+            {email}
+            {password}
+        </p>
+    )
+}
